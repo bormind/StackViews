ab5dd201751a78688ca0ca98eee345820c641839
Boris Schneiderman <you@example.com>
2017-03-23 09:23:57 -0700

    Working on readme

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # StackViews
 Small library for assigning auto layout constraints using declarative Stack View/Flex-Box semantics.
 
-# Motivation
+## Motivation
 There are many reasons for UI Reach production strength maintainable IOS applications to lean towards code based layouting and use storyboards searingly
 But programmatic constraints assignment is not very intuitive.
 There are may libraries that are trying to address this issue. Some created better syntax for assigning layout constraints, some completely bypassing autolayout engine and render views using there own engine.
@@ -10,3 +10,43 @@ This library consists of one major function: *stackViews(...)* and it is trying 
 It is similar to the IOS UIStackView but it works with any view and includes constraining of children view as a part of the same function call and has some options "borrowed" from the flex-box layout engine
 
 Bottom line - one function call includes all the aspects of constraining children inside the StackView
+
+## Example
+
+Fragment that generates this form header:<br>
+<img src="../images/form_header.png">
+<br>
+```Swift
+    let image = UIImageView(image: UIImage(named: "PersonProfile"))
+    let firstName = UITextField()
+    let middleName = UITextField()
+    let lastName = UITextField()
+
+    //Stack Fields vertically
+    let fieldsStackView = stackViews(
+            orientation: .vertical,
+            justify: .spaceBetween,
+            align: .fill,
+            views: [
+                    applyLabel("First Name", ofWidth: 110, toField: firstName),
+                    applyLabel("Middle Name", ofWidth: 110, toField: middleName),
+                    applyLabel("Last Name", ofWidth: 110, toField: lastName)
+            ],
+            heights: [25, 25, 25])
+        .container
+
+    //Stack image and fields horizontally
+    _ = stackViews(
+            container: self.view,
+            orientation: .horizontal,
+            justify: .fill,
+            align: .fill,
+            insets: Insets(horizontal: 5, vertical: 5),
+            spacing: 10,
+            views: [image, fieldsStackView],
+            widths: [100, nil])
+
+    //set image view to be square
+    image.widthAnchor.constraint(equalTo: image.heightAnchor, multiplier: 1).isActive = true
+
+```
--- StackViewsExamples/StackViewsExamples/DocsStackViewExapmle/DocsExampleHeaderViewController.swift
+++ StackViewsExamples/StackViewsExamples/DocsStackViewExapmle/DocsExampleHeaderViewController.swift
@@ -7,32 +7,33 @@ import Foundation
 import UIKit
 import StackViews
 
+fileprivate func formatTextField(_ textField: UITextField) {
+    textField.borderStyle = .roundedRect
+}
+
 class DocsExampleHeaderViewController: UIViewController {
 
-    let icon = UIImageView(image: UIImage(named: "PersonProfile"))
+    let image = UIImageView(image: UIImage(named: "PersonProfile"))
     let firstName = UITextField()
     let middleName = UITextField()
     let lastName = UITextField()
 
     init() {
         super.init(nibName: nil, bundle: nil)
-        self.view.backgroundColor = UIColor.white
-
-        //set image view to be square
-        icon.widthAnchor.constraint(equalTo: icon.heightAnchor, multiplier: 1).isActive = true
+        self.view.backgroundColor = UIColor.steelBlue
 
         //Set border for better visibility
-        [firstName, middleName, lastName].forEach { $0.borderStyle = .roundedRect }
+        [firstName, middleName, lastName].forEach(formatTextField)
 
         //Stack Fields vertically
-        let fieldsView = stackViews(
+        let fieldsStackView = stackViews(
                 orientation: .vertical,
                 justify: .spaceBetween,
                 align: .fill,
                 views: [
-                        applyLabel("First Name", ofWidth: 120, toField: firstName),
-                        applyLabel("Middle Name", ofWidth: 120, toField: middleName),
-                        applyLabel("Last Name", ofWidth: 120, toField: lastName)
+                        applyLabel("First Name", ofWidth: 110, toField: firstName),
+                        applyLabel("Middle Name", ofWidth: 110, toField: middleName),
+                        applyLabel("Last Name", ofWidth: 110, toField: lastName)
                 ],
                 heights: [25, 25, 25])
             .container
@@ -45,10 +46,11 @@ class DocsExampleHeaderViewController: UIViewController {
                 align: .fill,
                 insets: Insets(horizontal: 5, vertical: 5),
                 spacing: 10,
-                views: [icon, fieldsView],
-                //make icon width of 100
+                views: [image, fieldsStackView],
                 widths: [100, nil])
 
+        //set image view to be square
+        image.widthAnchor.constraint(equalTo: image.heightAnchor, multiplier: 1).isActive = true
 
     }
 
